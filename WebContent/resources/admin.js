1190acef32c6639b71c56249b1bf24f1979e138d
openchain-visolve <openchain.git@visolve.com>
2019-02-11 17:54:04 +0530

    update questionnnaire and error dialog formatting fix

--- WebContent/resources/admin.js
+++ WebContent/resources/admin.js
@@ -46,6 +46,7 @@ function updateSurvey() {
 	    type: "GET",
 	    dataType : "json",
 	    success: function( json ) {
+	    	
 	    	lastUpdateCommit = json.commit;
 	    	var summaryHtml = "";
 	    	if ( json.warnings.length > 0 ) {
@@ -114,11 +115,11 @@ function updateSurveyForReal( commit ) {
 	    	$( "#btUpdateSurvey" ).button( "enable" );
 	    	if ( json.status == "OK" ) {
 	    		var dialogText = json.surveyUpdateResult.versionsUpdated.length.toString();
-	    		dialogText += "<p class='translate' data-18n='questionnaire-updated'>questionnaire version/languages were updated</p>; ";
+	    		dialogText += "<p class='translate' data-i18n='questionnaire-updated'>questionnaire version/languages were updated</p>; ";
 	    		dialogText += json.surveyUpdateResult.versionsAdded.length.toString();
-	    		dialogText += "<p class='translate' data-18n='questionnaire-added'>questionnaire version/languages were added </p>";
+	    		dialogText += "<p class='translate' data-i18n='questionnaire-added'>questionnaire version/languages were added </p>";
 	    		if ( json.surveyUpdateResult.warnings.length > 0 ) {
-	    			dialogText += "<p class='translate' data-18n='questionnaire-warning'> with the following warnings <span>:</span></p>";
+	    			dialogText += "<p class='translate' data-i18n='questionnaire-warning'> with the following warnings <span>:</span></p>";
 	    			for ( i in json.surveyUpdateResult.warnings ) {
 	    				dialogText += " '";
 	    				dialogText += json.surveyUpdateResult.warnings[i];
@@ -427,15 +428,34 @@ $(document).ready( function() {
 	  }
 	});
 	
+	
+	
 	$( "#confirm-update-tabs" ).tabs();
 	
-	
-	
-	$("#btUpdateSurvey").button().button().click(function(event) {
-	      event.preventDefault();
-	      updateSurvey();
+	$( "#btUpdateSurvey" ).click(function() {
+			event.preventDefault();
+			var gitversion = $('select.git-select').find(":selected").val();
+			
+			if ( gitversion == "None" ) {
+				$('#UpdateSurveyModal').modal('hide');
+				
+				$.notify({
+					message: '<span class="translate" data-i18n="update-warning"> Select git version </span>',
+					icon: "fa fa-warning",
+				},{
+					type: 'warning'
+				});
+			
+			    $('select.git-select').focus();
+			}
+			else {
+				updateSurvey();
+				$('#UpdateSurveyModal').modal('show');
+			}
+			$('.translate').localize();
+			
 	});
-	
+
 	$("#btApprove").button().button().click(function(event) {
 	      event.preventDefault();
 	      requestStatusChange("setApproved", getCheckedIds($("#submitted-awaiting-approval")));
--- WebContent/resources/global.js
+++ WebContent/resources/global.js
@@ -50,13 +50,19 @@ var LANGUAGES = {"en":"English",
 			function displayError( error ) {
 				
 				event.preventDefault();
-			    $( "#errors" ).dialog().data( "uiDialog" )._title = function(title) {
-			  	    title.html( this.options.title );
-			  	};	
+			   	$.widget("ui.dialog", $.extend({}, $.ui.dialog.prototype, {
+				    _title: function(title) {
+				        if (!this.options.title ) {
+				            title.html("&#160;");
+				        } else {
+				            title.html(this.options.title);
+				        }
+				    }
+				}));
 			  	
 			  	
 				$( "#errors" ).dialog({
-					title: '<span class="translate" data-i18n="Error">Error</span>',
+					title:'<span class="translate" data-i18n="Error">Error</span>',
 					resizable: false,
 				    height: 250,
 				    width: 300,
